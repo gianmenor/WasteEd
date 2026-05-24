ac21d02258ebe89b1bfab61877c2d351e511dcf2
gianmenor <tmc.giancarlo.menor@cvsu.edu.ph>
2026-05-25 00:57:35 +0800

    Remove git rebase edit script

--- server/scripts/git-rebase-edit.ps1
+++
@@ -1,4 +0,0 @@
-param([string]$filePath)
-$contents = Get-Content -LiteralPath $filePath
-$contents = $contents -replace '^pick 3d54a59', 'edit 3d54a59'
-Set-Content -LiteralPath $filePath -Value $contents
